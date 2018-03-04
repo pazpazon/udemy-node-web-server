a9770fe98f166604c0e4d833fa5442ad650b3d09
pazpazon <35139232+pazpazon@users.noreply.github.com>
2018-03-04 19:13:55 +0100

    Initial commit.

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use( (req, res,  next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log',log + '\n', (err) => {
+    if (err) console.log('Could nor write to log!');
+  });
+
+  next();
+});
+
+app.use( (req, res,  next) => {
+  res.render('maintenance.hbs', {
+    pageTitle: 'Maintenance...',
+    maintenanceMessage: 'The site is currently down for maintenance... Please check back soon...'
+  });
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+hbs.registerHelper('screamIt', (text) => text.toUpperCase());
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home page',
+    welcomeMessage: 'Welcome to our website, wanker!'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: "About page"
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to display that page!'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server up on port 3000!');
+});
